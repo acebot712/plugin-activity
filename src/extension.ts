fd85eb77ffdbd74df71224671ab04d86d5ac3122
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-04-04 15:57:30 +0530

    code clean

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 import * as vscode from "vscode";
-import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
 	// register command handlers
@@ -8,32 +7,11 @@ export function activate(context: vscode.ExtensionContext) {
 		"myWebView",
 		"My Web View",
 		vscode.ViewColumn.One,
-		{ enableScripts: true }
+		{}
 	);
-
-	myWebView.onDidChangeViewState(e => {
-		myWebView.iconPath = vscode.Uri.file(
-			path.join(context.extensionPath, 'resources','icon.png')
-		);
-	});
-
-	if (!myWebView) {
-		console.log("No Web View");
-	}
-	console.log("Web View");
-	console.log(myWebView);
-
 
 	myWebView.webview.html = getWebviewContent();
 
-	const showWebViewCommand = vscode.commands.registerCommand(
-		"extension.showWebView",
-		() => {
-			myWebView.reveal(vscode.ViewColumn.One);
-		}
-	);
-
-	context.subscriptions.push(showWebViewCommand);
 }
 
 function getWebviewContent() {
